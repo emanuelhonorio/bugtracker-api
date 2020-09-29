5c5e8eea6f8bda164293213b406111186f9f00f1
Emanuel Honorio <emanuelh.dev@gmail.com>
2020-09-29 03:11:15 -0300

    process.end.PORT misspelled

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = require("./app");
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 require("../database");
 
 app.listen(port, () => {
-  console.log(`Server is running at http//localhost:${port}`);
+  console.log(`Server is running`);
 });
