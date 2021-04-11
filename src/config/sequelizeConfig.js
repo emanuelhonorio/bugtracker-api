6370c07b755b1fd90250c6db77c272806d38ab88
emanuelhonorio <43287989+emanuelhonorio@users.noreply.github.com>
2021-04-11 04:55:15 -0300

    Update sequelizeConfig.js

--- src/config/sequelizeConfig.js
+++ src/config/sequelizeConfig.js
@@ -10,8 +10,9 @@ module.exports = {
   dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false 
+        rejectUnauthorized: false,
       },
+  },
   define: {
     timestamps: true,
     underscored: true,
